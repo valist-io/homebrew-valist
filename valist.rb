8b87d57a6e6dc9d6fcecd4339d60efd4d7180c31
goreleaserbot <goreleaser@carlosbecker.com>
2021-12-01 16:01:19 -0500

    Brew formula update for valist version v0.6.1

--- valist.rb
+++ valist.rb
@@ -5,21 +5,21 @@
 class Valist < Formula
   desc "Universal package repositories, code signing, and access control."
   homepage "https://valist.io"
-  version "0.6.0"
+  version "0.6.1"
   license "MPL-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/valist-io/valist/releases/download/v0.6.0/valist_0.6.0_darwin_arm64.tar.gz"
-      sha256 "aa975c566f9a16bc11ea54b1ab0effc1af0b9bfdf76664c4a799871e64763d23"
+      url "https://github.com/valist-io/valist/releases/download/v0.6.1/valist_0.6.1_darwin_arm64.tar.gz"
+      sha256 "a17f42d83b4b4a557f312673c68c0c339356a13f7d1740092372fb392d086f2e"
 
       def install
         bin.install "valist"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/valist-io/valist/releases/download/v0.6.0/valist_0.6.0_darwin_amd64.tar.gz"
-      sha256 "1a1fa3a2d4a6ccce88cfc625aa291f61cc1a6a772195985bf7c497be96150e69"
+      url "https://github.com/valist-io/valist/releases/download/v0.6.1/valist_0.6.1_darwin_amd64.tar.gz"
+      sha256 "9140dedff59e0203817f701215681907532a1a06419566caf6dd34490173c515"
 
       def install
         bin.install "valist"
@@ -29,16 +29,16 @@ class Valist < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/valist-io/valist/releases/download/v0.6.0/valist_0.6.0_linux_arm64.tar.gz"
-      sha256 "081016effd85a319fa760fa35b74244e581d70e69820505272e34587664cf8db"
+      url "https://github.com/valist-io/valist/releases/download/v0.6.1/valist_0.6.1_linux_arm64.tar.gz"
+      sha256 "d73a0786b6b1fd2c5d5006098a89d49d459632423613bef6bd2913e4185b2298"
 
       def install
         bin.install "valist"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/valist-io/valist/releases/download/v0.6.0/valist_0.6.0_linux_amd64.tar.gz"
-      sha256 "0cc7dfe1cd50bacc0ef4c0e287a04e70e499448dfce6fc24a8f7538cc1e436ba"
+      url "https://github.com/valist-io/valist/releases/download/v0.6.1/valist_0.6.1_linux_amd64.tar.gz"
+      sha256 "39e24f8152f98743d152f8822118de6bec484dbf3f46c3edd16617ba6f3ef003"
 
       def install
         bin.install "valist"
